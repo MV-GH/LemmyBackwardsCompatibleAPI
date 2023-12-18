897869cd0bda4c49c568289758da96c9c9760200
maarten.vercruysse <maarten.vercruysse@outlook.be>
2023-12-18 19:05:47 +0100

    Fix lint

--- app/src/commonMain/kotlin/it/vercruysse/lemmyapi/pictrs/PictrsService.kt
+++ app/src/commonMain/kotlin/it/vercruysse/lemmyapi/pictrs/PictrsService.kt
@@ -46,12 +46,12 @@ open class PictrsService(private val ktor: HttpClient, version: Version, overrid
 
             imageResp.copy(
                 files =
-                imageResp.files.map {
-                    it.copy(
-                        url = "${resp.call.request.url}/${it.file}",
-                        delete_url = "${resp.call.request.url}/delete/${it.delete_token}/${it.file}",
-                    )
-                },
+                    imageResp.files.map {
+                        it.copy(
+                            url = "${resp.call.request.url}/${it.file}",
+                            delete_url = "${resp.call.request.url}/delete/${it.delete_token}/${it.file}",
+                        )
+                    },
             )
         }
     }
