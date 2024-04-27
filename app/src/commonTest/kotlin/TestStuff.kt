13aff857d7e7bd1e7de53814848d208fe241721a
maarten.vercruysse <maarten.vercruysse@outlook.be>
2024-04-28 00:29:06 +0200

    Small change

--- app/src/commonTest/kotlin/TestStuff.kt
+++ app/src/commonTest/kotlin/TestStuff.kt
@@ -1,5 +1,6 @@
 import io.ktor.http.*
 import it.vercruysse.lemmyapi.LemmyApi
+import it.vercruysse.lemmyapi.v0x19.datatypes.GetPosts
 import it.vercruysse.lemmyapi.v0x19.datatypes.ListMedia
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
@@ -13,10 +14,11 @@ class TestStuff {
 
     @Test
     fun test2() = runTest {
-        val api = LemmyApi.getLemmyApi("voyager.lemmy.ml", auth = VOYAGER_AUTH)
+        val api = LemmyApi.getLemmyApi("lemmy.ml")
 
         println(api.version)
         println(api.getSite())
+        println(api.getPosts(GetPosts()))
         println(api.listMedia(ListMedia()))
     }
 }
