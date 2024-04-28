650a936c963276c4f48aab0ff264524a20b90abf
Maarten Vercruysse <67873169+MV-GH@users.noreply.github.com>
2024-04-28 22:37:06 +0200

    Use int for createLike scores (#44)

--- app/src/commonMain/kotlin/it/vercruysse/lemmyapi/v0x18/datatypes/CreatePostLike.kt
+++ app/src/commonMain/kotlin/it/vercruysse/lemmyapi/v0x18/datatypes/CreatePostLike.kt
@@ -5,6 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 internal data class CreatePostLike(
     val post_id: PostId,
-    val score: Long,
+    val score: Int,
     val auth: String,
 )
