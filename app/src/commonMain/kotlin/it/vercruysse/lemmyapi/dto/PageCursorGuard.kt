8c2a0eea8ecaab4a500320e5935112c7d6b3255d
maarten.vercruysse <maarten.vercruysse@outlook.be>
2024-06-15 22:34:13 +0200

    Fix comment

--- app/src/commonMain/kotlin/it/vercruysse/lemmyapi/dto/PageCursorGuard.kt
+++ app/src/commonMain/kotlin/it/vercruysse/lemmyapi/dto/PageCursorGuard.kt
@@ -1,4 +1,4 @@
 package it.vercruysse.lemmyapi.dto
 
-// Assumed that there will neve a page cursor returned that is equal
+// There should never be a page cursor returned that is the same as this
 val PAGE_CURSOR_GUARD = "$$$$$$$$$$$$$$$$$$$$$$$$$$$"
