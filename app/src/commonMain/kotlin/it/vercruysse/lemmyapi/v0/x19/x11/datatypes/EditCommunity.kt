0b4d39712651f871264c6ef489e7815ad65f94bf
iByteABit256 <paulsmith4561@gmail.com>
2025-04-26 16:21:19 +0300

    Changed visibility from String to CommunityVisibility type

--- app/src/commonMain/kotlin/it/vercruysse/lemmyapi/v0/x19/x11/datatypes/EditCommunity.kt
+++ app/src/commonMain/kotlin/it/vercruysse/lemmyapi/v0/x19/x11/datatypes/EditCommunity.kt
@@ -1,5 +1,6 @@
 package it.vercruysse.lemmyapi.v0.x19.x11.datatypes
 
+import it.vercruysse.lemmyapi.dto.CommunityVisibility
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -12,5 +13,5 @@ internal data class EditCommunity(
     val nsfw: Boolean? = null,
     val posting_restricted_to_mods: Boolean? = null,
     val discussion_languages: List<LanguageId>? = null,
-    val visibility: String? /* "Public" | "LocalOnly" */ = null,
+    val visibility: CommunityVisibility? /* "Public" | "LocalOnly" */ = null,
 )
