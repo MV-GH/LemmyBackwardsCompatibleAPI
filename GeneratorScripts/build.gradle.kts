d1cf674447bc9bb842eafa399118379c26e953c6
Maarten Vercruysse <67873169+MV-GH@users.noreply.github.com>
2025-04-30 00:43:44 +0200

    Bump deps (#83)

--- GeneratorScripts/build.gradle.kts
+++ GeneratorScripts/build.gradle.kts
@@ -11,15 +11,15 @@ repositories {
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     implementation("com.charleskorn.kaml:kaml:0.55.0")
-    val ktorVersion = "3.1.0"
+    val ktorVersion = "3.1.2"
     val konvertVersion = "3.2.1"
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
 
     implementation("com.squareup.okio:okio:3.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-io-core:0.6.0")
