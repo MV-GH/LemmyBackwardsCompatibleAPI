b8947be76caef0446b1e30f86eafc038020c12b9
maarten.vercruysse <maarten.vercruysse@outlook.be>
2026-03-08 12:35:34 +0100

    Small tweaks

--- app/src/commonMain/kotlin/it/vercruysse/lemmyapi/datatypes/ListPersonContent.kt
+++ app/src/commonMain/kotlin/it/vercruysse/lemmyapi/datatypes/ListPersonContent.kt
@@ -2,13 +2,13 @@ package it.vercruysse.lemmyapi.datatypes
 
 import it.vercruysse.lemmyapi.CommonParcelize
 import it.vercruysse.lemmyapi.DatatypeRoot
+import it.vercruysse.lemmyapi.enums.PersonContentType
 import kotlinx.serialization.Serializable
 
 @CommonParcelize
 @Serializable
 data class ListPersonContent(
-    // TODO: custom enum
-    val type_: String? /* "all" | "comments" | "posts" */ = null,
+    val type_: PersonContentType? /* "all" | "comments" | "posts" */ = null,
     val person_id: PersonId? = null,
     val username: String? = null,
     val page_cursor: PaginationCursor? = null,
