0a9221560b341687a5316bf6bdcfe22b807a078b
maarten.vercruysse <maarten.vercruysse@outlook.be>
2026-07-18 00:47:15 +0200

    Move base routes to UniRoutes

--- app/src/commonMain/kotlin/it/vercruysse/lemmyapi/UniRoutes.kt
+++ app/src/commonMain/kotlin/it/vercruysse/lemmyapi/UniRoutes.kt
@@ -1,34 +1,10 @@
 package it.vercruysse.lemmyapi
 
-import io.github.z4kn4fein.semver.Version
 import it.vercruysse.lemmyapi.datatypes.*
 import it.vercruysse.lemmyapi.dto.ExportUserSettingsResponse
 import it.vercruysse.lemmyapi.dto.ImportUserSettings
-import it.vercruysse.lemmyapi.utils.runCatchingPreservingCancellation
 
-abstract class LemmyApiBaseController(actualVersion: Version, baseUrl: String, override var auth: String?) :
-    LemmyApiBase(
-        actualVersion,
-        baseUrl,
-        auth,
-    ),
-    OldRoutes {
-
-    suspend fun uploadAndApplyImage(
-        image: ByteArray,
-        applyImage: suspend (String) -> Result<Unit>,
-    ): Result<UploadImageResponse> = runCatchingPreservingCancellation {
-        val uploadResponse = uploadImage(image).getOrThrow()
-        try {
-            applyImage(uploadResponse.image_url).getOrThrow()
-            uploadResponse
-        } catch (e: Throwable) {
-            deleteMedia(DeleteImageParams(uploadResponse.delete_filename)).getOrNull()
-            throw e
-        }
-    }
-
-    // TODO: Move these routes to UniRoutes but preserve Git History
+interface UniRoutes {
 
     /**
      * Gets the site, and your user data.
