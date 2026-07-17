ec711c1e06aa2dfd7c6e069f194a9c8fe87e57bb
maarten.vercruysse <maarten.vercruysse@outlook.be>
2026-07-18 00:55:05 +0200

    Remove LemmyApiBase.kt

--- app/src/commonMain/kotlin/it/vercruysse/lemmyapi/LemmyApiBaseController.kt
+++ app/src/commonMain/kotlin/it/vercruysse/lemmyapi/LemmyApiBaseController.kt
@@ -3,16 +3,37 @@ package it.vercruysse.lemmyapi
 import io.github.z4kn4fein.semver.Version
 import it.vercruysse.lemmyapi.datatypes.DeleteImageParams
 import it.vercruysse.lemmyapi.datatypes.UploadImageResponse
+import it.vercruysse.lemmyapi.dto.getSupportedEntries
+import it.vercruysse.lemmyapi.enums.VersionTracker
+import it.vercruysse.lemmyapi.exception.NotSupportedException
 import it.vercruysse.lemmyapi.utils.runCatchingPreservingCancellation
 
-abstract class LemmyApiBaseController(actualVersion: Version, baseUrl: String, override var auth: String?) :
-    LemmyApiBase(
-        actualVersion,
-        baseUrl,
-        auth,
-    ),
+abstract class LemmyApiBaseController(
+    val version: Version,
+    val baseUrl: String,
+    open var auth: String?,
+) :
     UniRoutes,
     OldRoutes {
+    @Suppress("PropertyName")
+    val FF = FeatureFlags(version)
+
+    /**
+     * Returns the supported entries enum entries for this API version.
+     *
+     * It is possible that this list is empty, such case means this type is not used at all in a newer version
+     * Or doesn't exist in the older version.
+     *
+     * @return A list of supported entries
+     */
+    // TODO: add used coverage
+    inline fun <reified T> getSupportedEntries(): List<T> where T : Enum<T>, T : VersionTracker = getSupportedEntries(version)
+
+    protected inline fun <reified T> notSupported(): Result<T> = Result.failure(
+        NotSupportedException(
+            "This endpoint is not supported on this version of Lemmy: $version, use a FeatureFlag to check if it's supported",
+        ),
+    )
 
     suspend fun uploadAndApplyImage(
         image: ByteArray,
