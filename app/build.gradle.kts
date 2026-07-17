b6ed77486189b1d59d8a367b6d1cc4decc5bb1e0
maarten.vercruysse <maarten.vercruysse@outlook.be>
2026-07-18 01:58:25 +0200

    Bump kotlinter version to 5.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jmailen.gradle.kotlinter.tasks.LintTask
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization") version "2.3.21"
-    id("org.jmailen.kotlinter") version "5.5.0"
+    id("org.jmailen.kotlinter") version "5.6.0"
     id("com.vanniktech.maven.publish") version "0.37.0"
     id("com.github.ben-manes.versions") version "0.54.0"
     id("com.android.kotlin.multiplatform.library")
